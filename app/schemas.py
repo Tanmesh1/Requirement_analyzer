94664c01040efd44a2c7cd22932861cb39b54857
Tanmesh Joshi <tanmeshj2002@gamil.com>
2026-02-09 14:33:20 +0530

    phase 2 started and crated schemas

---
+++ app/schemas.py
@@ -0,0 +1,32 @@
+from pydantic import BaseModel , EmailStr
+from datetime import datetime
+
+#---------------------------
+# User Schema
+#---------------------------
+
+class UserBase(BaseModel):
+    email: EmailStr
+
+class UserCreate(UserBase):
+    password: str
+
+class UserResponse(UserBase):
+    id: int
+    created_at : datetime
+
+    class Config:
+        from_attributes = True
+
+
+#--------------------
+# Auth Schema
+#--------------------
+
+class Token(BaseModel):
+    access_token: str
+    token_type: str ="bearer"
+
+class LoginRequest(BaseModel):
+    email : EmailStr
+    password: str
